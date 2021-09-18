d45eddf5b69435e8ed18ca265056ca27d2f10fa5
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-18 15:54:11 -0500

    @app_views

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,3 +5,22 @@ Flask route returns json status
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """
+    function to return the count of all class objects
+    """
+    if request.method == 'GET':
+        response = {}
+        PLURALS = {
+            "Amenity": "amenities",
+            "City": "cities",
+            "Place": "places",
+            "Review": "reviews",
+            "State": "states",
+            "User": "users"
+        }
+        for key, value in PLURALS.items():
+            response[value] = storage.count(key)
+        return jsonify(response)

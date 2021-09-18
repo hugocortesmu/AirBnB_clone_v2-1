15bfef4c920d05c7c437c31bb30db8961d8225af
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-18 15:29:34 -0500

    host and port

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python3
 
-import os 
+import os
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask import Blueprint
 from flask_cors import CORS
 app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+app.register_blueprint(app_views)
+
+if __name__ == "__main__":
+    app.run(host=os.getenv('HBNB_API_HOST', "0.0.0.0"),
+            port=os.getenv('HBNB_API_PORT', "5000"), threaded=True)

68dc6195dffeb4b3fa71e78688d4ce7290b06dc1
luzbaza <2459@holbertonschool.com>
2021-09-20 18:25:37 -0500

    modified import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,21 @@
 """
 Flask route returns json status
 """
+
+from flask.app import Flask
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+app = Flask(__name__)
+
 
 @app_views.route('/status', strict_slashes=False)
 def status():

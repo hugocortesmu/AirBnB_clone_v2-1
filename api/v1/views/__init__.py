c2b19671343feee5360526c13969d282ea4d01bd
luzbaza <2459@holbertonschool.com>
2021-09-20 18:17:37 -0500

    modifed flask

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,8 +2,8 @@
 """
 blueprint instances
 """
-import flask
+from flask import Blueprint
 
-app_views = flask.Blueprint("views", __name__, url_prefix="/api/v1")
+app_views = Blueprint("views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *

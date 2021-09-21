c344ac88333c0725432b3d4078764340a3c25c15
luzbaza <2459@holbertonschool.com>
2021-09-21 03:27:17 +0000

    add states

---
+++ api/v1/views/states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Flask route that returns json status response
+"""
+from api.v1.views import app_views
+from flask import abort, jsonify, make_response, request
+from flasgger import Swagger, swag_from
+from models import storage, CNC
+
+
+@app_views.route('/states', methods=['GET', 'POST'])
+@swag_from('swagger_yaml/states_no_id.yml', methods=['GET', 'POST'])
+def states_no_id():
+    """
+        states route to handle http method for requested states no id provided
+    """
+    if request.method == 'GET':
+        all_states = storage.all('State')
+        all_states = list(obj.to_json() for obj in all_states.values())
+        return jsonify(all_states)
+
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get("name") is None:
+            abort(400, 'Missing name')
+        State = CNC.get("State")
+        new_object = State(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201

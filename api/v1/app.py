abbf5f94372677a5ef8769baac6d8226e488830c
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-20 19:38:07 -0500

    cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,12 +3,16 @@
 Flask App integrate with HTML
 """
 import os
-from flask import Flask, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask import Blueprint
 from flask_cors import CORS
+from werkzeug.exceptions import HTTPException
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
+app.config["DEBUG"] = True
 
 @app.teardown_appcontext
 def teardown_db(exception):
@@ -20,7 +24,7 @@ def teardown_db(exception):
 
 app.register_blueprint(app_views)
 
-if __name__ == "__main__":
-	app.run(host=os.getenv('HBNB_API_HOST') or '0.0.0.0',
-	        port=os.getenv('HBNB_API_HOST') or '5000',
-			threaded=True)
+@app.errorhandler(404)
+def erro_not_found(error):
+    """status code response whit handler for 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)

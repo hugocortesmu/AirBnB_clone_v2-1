bf5807b3720f1d0a54dad6753ca6bdc870bcdc47
Hugo Cortes <hugocortesmu@gmail.com>
2021-09-20 23:51:13 -0500

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,12 @@ Flask App integrate with HTML
 """
 import os
 from os import getenv
-from flask import Flask, jsonify
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from flask import Blueprint
 from flask import CORS
+from werkzeug.exceptions import HTTPException
 app = Flask(__name__)
 
 @app.teardown_appcontext
@@ -21,6 +22,10 @@ def teardown_db(exception):
 
 app.register_blueprint(app_views)
 
+@app.errorhandler(404)
+def erro_not_found(error):
+    """status code response whit handler for 404 errors"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == "__main__":
     app.run(host=getenv(

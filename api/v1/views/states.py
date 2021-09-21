f9ffe9c38be900080d84249f359db6dfa5ef74be
luzbaza <2459@holbertonschool.com>
2021-09-21 03:34:00 +0000

    add states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,3 +29,28 @@ def states_no_id():
         new_object = State(**req_json)
         new_object.save()
         return jsonify(new_object.to_json()), 201
+
+@app_views.route('/states/<state_id>', methods=['GET', 'DELETE', 'PUT'])
+@swag_from('swagger_yaml/states_id.yml', methods=['PUT', 'GET', 'DELETE'])
+def states_with_id(state_id=None):
+    """
+        states route to handle http method for requested state by id
+    """
+    state_obj = storage.get('State', state_id)
+    if state_obj is None:
+        abort(404, 'Not found')
+
+    if request.method == 'GET':
+        return jsonify(state_obj.to_json())
+
+    if request.method == 'DELETE':
+        state_obj.delete()
+        del state_obj
+        return jsonify({})
+
+    if request.method == 'PUT':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        state_obj.bm_update(req_json)
+        return jsonify(state_obj.to_json())
